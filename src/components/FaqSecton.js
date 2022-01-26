427ac7d3da3c7a9a1698d3f70023ddbb29c6617d
Ravikumar Patel <UP0165@uplers.local>
2022-01-27 01:46:21 +0530

    FAQ section html

---
+++ src/components/FaqSecton.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions? <span>FAQ</span></h2>
+            <div className="question">
+                <h4>HOw to do start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Excepturi nisi iusto architecto pariatur distinctio cupiditate tempore aperiam fugit facere labore. Itaque delectus perspiciatis vero est, excepturi autem error cum ipsum?</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Excepturi nisi iusto architecto pariatur distinctio cupiditate tempore aperiam fugit facere labore. Itaque delectus perspiciatis vero est, excepturi autem error cum ipsum?</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Whts new payment options?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Excepturi nisi iusto architecto pariatur distinctio cupiditate tempore aperiam fugit facere labore. Itaque delectus perspiciatis vero est, excepturi autem error cum ipsum?</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>HOw to do start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Excepturi nisi iusto architecto pariatur distinctio cupiditate tempore aperiam fugit facere labore. Itaque delectus perspiciatis vero est, excepturi autem error cum ipsum?</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection;
