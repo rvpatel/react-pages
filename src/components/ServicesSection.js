7426046eb331446b33ec09208218910da71fac21
Ravikumar Patel <UP0165@uplers.local>
2022-01-28 00:33:05 +0530

    services styles

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -5,47 +5,80 @@ import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
 import home2 from '../img/home2.png';
 
+//styled
+import styled from "styled-components";
+import { About, Description, Image } from "../styles";
+
 const ServicesSection = () => {
     return (
-        <div className="services">
-            <div className="discription">
+        <Services>
+            <Description>
                 <h2>HIgh <span>quality</span> services</h2>
-                <div className="cards">
-                    <div className="card">
+                <Cards>
+                    <Card>
                         <div className="icon">
                             <img src={clock} alt="clock" />
                             <h3>Efficient</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    </div>
-                    <div className="card">
+                    </Card>
+                    <Card>
                         <div className="icon">
-                            <img src={diaphram} alt="diaphram"/>
+                            <img src={diaphram} alt="diaphram" />
                             <h3>Efficient</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    </div>
-                    <div className="card">
+                    </Card>
+                    <Card>
                         <div className="icon">
-                            <img src={teamwork} alt="teamwork"/>
+                            <img src={teamwork} alt="teamwork" />
                             <h3>Efficient</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    </div>
-                    <div className="card">
+                    </Card>
+                    <Card>
                         <div className="icon">
                             <img src={money} alt="money" />
                             <h3>Efficient</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    </div>
-                </div>
-                <div className="image">
-                    <img src={home2} alt="home2"/>
-                </div>
-            </div>
-        </div>
-    )
-}
+                    </Card>
+                </Cards>
+            </Description>
+            <Image>
+                <img src={home2} alt="home2" />
+            </Image>
+        </Services>
+    );
+};
+
+const Services = styled(About)`
+    h2{
+        padding-bottom: 5rem;
+    }
+    p{
+        width: 70%;
+        padding: 2rem 0rem 4rem 0rem;
+    }
+`;
+
+const Cards = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+    flex-basis: 20rem;
+    .icon {
+        display: flex;
+        align-items: center;
+    }
+    h3{
+        margin-left: 1rem;
+        background: white;
+        color: black;
+        padding: 1rem;
+    }
+`;
 
 export default ServicesSection;
