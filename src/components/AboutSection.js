dc893fbbcafefbaba74287aa65ab09040d24accc
Ravikumar Patel <UP0165@uplers.local>
2022-02-08 03:35:47 +0530

    Added about us page animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,6 +5,7 @@ import home1 from '../img/home1.png';
 import { motion } from 'framer-motion/dist/framer-motion'
 //styled
 import { About, Hide, Description, Image } from "../styles";
+import { titleAni, fade, photoAnim} from "../animation";
 
 const AboutSection = () => {
     return (
@@ -12,22 +13,22 @@ const AboutSection = () => {
             <Description>
                 <motion.div>
                     <Hide>
-                        <motion.h2>We work to make</motion.h2>
+                        <motion.h2 variants={titleAni}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAni}>
                             your <span>derems</span> come
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>true.</motion.h2>
+                        <motion.h2 variants={titleAni}>true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any photography or videography ideas that you have. Also have a professional amazing slills.</p>
-                <button>Contact Us</button>
+                <motion.p variants={ fade }>Contact us for any photography or videography ideas that you have. Also have a professional amazing slills.</motion.p>
+                <motion.button variants={ fade }>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with a camera" />
+                <motion.img variants={ photoAnim }src={home1} alt="guy with a camera" />
             </Image>
         </About>
     );
