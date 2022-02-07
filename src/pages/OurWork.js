82cac79824c3e024c838d2ef993690a171e070fb
Ravikumar Patel <UP0165@uplers.local>
2022-02-08 04:31:46 +0530

    Added Ourwork animation

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -9,22 +9,30 @@ import goodtimes from '../img/goodtimes-small.png';
 
 //Animation
 import { motion } from 'framer-motion/dist/framer-motion'
-import { pageAnimation } from "../animation";
+import { pageAnimation, fade, photoAnim, lineAnim, slider, sliderContainer } from "../animation";
 
 
 const OurWork = () => {
     return(
-        <Work style={{ background:"#fff" }} exit="exit" variants={pageAnimation} initial='hidden' animate='show'>
+        <Work style={{ background: "#fff" }} exit="exit" variants={pageAnimation} initial='hidden' animate='show'>
+            <motion.div variants={sliderContainer}>
+                <Frame1 variants={slider}></Frame1>
+                <Frame2 variants={slider}></Frame2>
+                <Frame3 variants={slider}></Frame3>
+                <Frame4 variants={slider}></Frame4>
+            </motion.div>
             <Movie>
-                <h2>The Athletes</h2>
-                <div className="line"></div>
+                <motion.h2 variants={fade}>The Athletes</motion.h2>
+                <motion.div className="line" variants={lineAnim}></motion.div>
                 <Link to="/work/the-athlete">
-                    <img src={athlet} alt="athlet" />
+                    <Hide>
+                        <motion.img variants={photoAnim} src={athlet} alt="athlet" />
+                    </Hide>
                 </Link>
             </Movie>
             <Movie>
-                <h2>The Racer</h2>
-                <div className="line"></div>
+                <motion.h2 variants={fade}>The Racer</motion.h2>
+                <div className="line" variants={lineAnim}></div>
                 <Link to="/work/the-racer">
                     <img src={theracer} alt="theracer" />
                 </Link>
@@ -52,8 +60,8 @@ const Work = styled(motion.div)`
 const Movie = styled.div`
     padding-bottom: 10rem;
     .line{
-        height: 0rem.5rem;
-        background: #cccccc;
+        height: 0.5rem;
+        background: #23dd97;
         margin-bottom: 3rem;
     }
     img{
@@ -62,5 +70,32 @@ const Movie = styled.div`
         object-fit: cover;
     }
 `;
+
+const Hide = styled.div`
+    overflow: hidden;
+`;
+
+const Frame1 = styled(motion.div)`
+    position: fixed;
+    left: 0;
+    top: 10%;
+    width: 100%;
+    height: 100vh;
+    background: #fffebf;
+    z-index: 2;
+`;
+
+const Frame2 = styled(Frame1)`
+    background: #ff8efb;
+`;
+
+const Frame3 = styled(Frame1)`
+    background: #8ed2ff;
+`;
+
+const Frame4 = styled(Frame1)`
+    background: #8effa0;
+`;
+
 
 export default OurWork;
